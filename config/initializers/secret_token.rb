067a46d3505880dd51780805ec5b006b9258926f
Brian Jordan <bcjordan@gmail.com>
2011-04-25 12:31:45 -0400

    gifttastic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wtfmom2::Application.config.secret_token = '8b254ad04c1a94e0eeec806ff988ea7e201283ee5ae4640c4d54ba641f03f5fef967d9150fb07b39fd82d0527771c6fdeea6885440ac351380eec6d861c8a56a'
