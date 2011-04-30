f20e3f723e388b18c064b148c569197f40cd845e
Brian Jordan <bcjordan@gmail.com>
2011-04-30 17:29:15 -0400

    add kissinsights box

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -10,4 +10,31 @@ module GiftsHelper
     url_for :only_path => false, :params => params.merge(overwrite)
   end
 
+  def session_increment
+    if session[:count]
+      session[:count] += 1
+    else
+      session[:count] = 0
+    end
+  end
+
+  def sc
+    session[:count].to_i
+  end
+
+  def save_referer
+    unless session['referer']
+      session['referer'] = request.env["HTTP_REFERER"] || 'none'
+    end
+  end
+
+  def ref
+    if session['referer']
+       session['referer']
+    else
+      "nowhere"
+    end
+  end
+    
+
 end
